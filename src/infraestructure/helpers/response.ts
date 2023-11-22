a93390fe8390faa9756fa608702f3f0c73324e6d
adavidarevalo <davidarevaloc20@gmail.com>
2023-11-22 07:36:56 -0500

    Fix: Schema

--- src/infraestructure/helpers/response.ts
+++ src/infraestructure/helpers/response.ts
@@ -1,6 +1,12 @@
 /** @format */
+import { StatusCodes } from 'http-status-codes';
 
-export const responseHttp = (success: boolean = true, data: {} = null, message: string = null, statusCode = 200) => {
+export const responseHttp = (
+  success: boolean = true,
+  data: {} = null,
+  message: string = null,
+  statusCode = StatusCodes.OK
+) => {
   return {
     statusCode,
     body: JSON.stringify({
--- src/infraestructure/schema/user_schema.ts
+++ src/infraestructure/schema/user_schema.ts
@@ -20,7 +20,8 @@ export const updateUserSchema = {
     lastName: { type: 'string', maxLength: 20 },
     birthDate: { type: 'string', format: 'date-time' },
     idType: { type: 'string', enum: ['cc', 'pas'] },
+    id: { type: 'string' },
     salary: { type: 'number' },
   },
-  required: [],
+  required: ['id'],
 };
